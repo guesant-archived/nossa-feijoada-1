40d7136e8172442bac4609505fd94e78e370eed6
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-05 13:39:25 -0400

    feijoada-ui: Esqueleto do HomeGallery

--- packages/feijoada-ui/src/components/GalleryItem.vue
+++ packages/feijoada-ui/src/components/GalleryItem.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
+  <div class="flex flex-col h-full">
     <div>
       <div class="p-2">
         <img class="h-24 md:h-32 w-full object-cover" :src="img.uri" :alt="img.alt" />
       </div>
     </div>
-    <div class="p-2">
+    <div class="flex-1">
       <slot name="body" />
+    </div>
+    <div>
+      <slot name="footer" />
     </div>
   </div>
 </template>
