983367082f18a71afad3b1dd4dc64cf48f2b5e34
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-05 08:21:20 -0400

    feijoada-ui: Componentes da galeria (home)

---
+++ packages/feijoada-ui/src/components/Gallery.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <slot name="wrap" />
+
+    <div class="px-4 md:px-16">
+      <slot name="before" />
+      <slot />
+      <slot name="after" />
+    </div>
+
+    <slot name="wrap" />
+  </section>
+</template>
---
+++ packages/feijoada-ui/src/components/GalleryItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <div class="p-2">
+        <img class="h-24 md:h-32 w-full object-cover" :src="img.uri" :alt="img.alt" />
+      </div>
+    </div>
+    <div class="p-2">
+      <slot name="body" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: { type: Object },
+  }
+}
+</script>
