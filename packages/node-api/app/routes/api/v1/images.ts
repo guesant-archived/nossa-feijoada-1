63007000e17f8902d8481b537c0b5aecc7ee2af9
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-09 11:13:37 -0400

    node-api: Limite para upload de imagens
    -> Caso exceda 9 requisições em uma hora,
    será bloqueada a rota `/api/v1/upload/:type`

--- packages/node-api/app/routes/api/v1/images.ts
+++ packages/node-api/app/routes/api/v1/images.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import auth from './auth';
 import * as controller from '../../../controllers/ImageController';
 
 const routes = express.Router();
 routes.post(
   '/upload/:type',
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 9,
+    message: JSON.stringify({
+      message:
+        'Muitas requisições vindas deste IP. Tente novamente em uma hora.',
+    }),
+  }),
   auth.required,
   ...controller.ControllerNew.default,
 );
