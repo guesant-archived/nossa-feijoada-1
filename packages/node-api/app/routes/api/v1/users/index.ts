48d56305e42911e267633a7ce9b9060ef4643d15
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-09 11:14:27 -0400

    node-api: Limite para login e cadastro
    -> Caso exceda 3 requisições em uma hora,
    será bloqueada a rota `/api/v1/users`
    
    -> Caso exceda 5 requisições em uma hora,
    será bloqueada a rota `/api/v1/users/login`

--- packages/node-api/app/routes/api/v1/users/index.ts
+++ packages/node-api/app/routes/api/v1/users/index.ts
@@ -1,19 +1,36 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import * as controller from '../../../../controllers/UsersController';
 import validationMiddleware from '../../../../middlewares/validation-middleware';
 
 const routes = express.Router();
 routes.post(
   '/',
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 3,
+    message: JSON.stringify({
+      message:
+        'Muitas requisições vindas deste IP. Tente novamente em uma hora.',
+    }),
+  }),
   controller.ControllerNew.validation,
   validationMiddleware,
-  controller.ControllerNew.default
+  controller.ControllerNew.default,
 );
 routes.post(
   '/login',
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: JSON.stringify({
+      message:
+        'Muitas requisições vindas deste IP. Tente novamente em 15 minutos.',
+    }),
+  }),
   controller.ControllerLogin.validation,
   validationMiddleware,
-  controller.ControllerLogin.default
+  controller.ControllerLogin.default,
 );
 
 export default routes;
