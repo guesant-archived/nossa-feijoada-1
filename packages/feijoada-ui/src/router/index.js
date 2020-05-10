c2778184e079f374fd8fe554c46c9bfbe78da559
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-10 09:13:57 -0400

    feijoada-ui: Corrige implementação do router

--- packages/feijoada-ui/src/router/index.js
+++ packages/feijoada-ui/src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
   const routes = [
   {
@@ -27,11 +27,12 @@ Vue.use(VueRouter)
     component: () =>
       import(/* webpackChunkName: "signup" */ '../views/Cadastro.vue'),
   },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
