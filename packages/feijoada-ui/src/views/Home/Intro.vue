864d5831c6d39bbb9370ebc62e4dc8150029eb9c
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-10 09:14:17 -0400

    feijoada-ui: Diminui o espaçamento da Home/Intro

--- packages/feijoada-ui/src/views/Home/Intro.vue
+++ packages/feijoada-ui/src/views/Home/Intro.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <div
-      class="content container h-48 mx-auto px-4 py-16 flex flex-col justify-center items-center"
+      class="content container mx-auto px-4 flex flex-col justify-center items-center"
     >
       <h1 class="text-white text-2xl sm:text-3xl md:text-4xl font-bold">BaianoSimulator</h1>
       <p class="mt-0 sm:mt-1 text-center text-white text-sm sm:text-base md:text-base">
