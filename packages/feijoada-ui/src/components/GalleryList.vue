c54179ad7f17e491f7f21291dad27f6a3130bbce
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-11 11:01:14 -0400

    feijoada-ui: Tira a classe não usada do GalleryList

--- packages/feijoada-ui/src/components/GalleryList.vue
+++ packages/feijoada-ui/src/components/GalleryList.vue
@@ -1,7 +1,5 @@
 <template>
-  <ul
-    class="container mx-auto max-w-lg w-full flex grid grid-cols-12 gap-4"
-  >
+  <ul class="container mx-auto max-w-lg w-full grid grid-cols-12 gap-4">
     <slot />
   </ul>
 </template>
