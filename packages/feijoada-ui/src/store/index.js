493bf297b0696e8bc775eeefdafc10f4c1f3b2f5
Gabriel Rodrigues <jipacoding@gmail.com>
2020-05-11 11:19:13 -0400

    feijoada-ui: Corrige o Vuex importa módulos requeridos pelo app

--- packages/feijoada-ui/src/store/index.js
+++ packages/feijoada-ui/src/store/index.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import settings from './modules/settings'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import settings from './modules/settings';
+import shared from './modules/shared';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
   modules: {
     settings,
+    shared,
   },
-  strict: debug
-})
+  strict: debug,
+});
